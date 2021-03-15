2a92e1352fc251e1b5ef9dc9bdd17deaf137af2a
zhoup-test <zhoup@startimes.com.cn>
2021-03-15 09:49:58 +0000

    修改：     config.tf

--- config.tf
+++ config.tf
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-provider "awesome" {
+provider "aws" {
   api_key     = "s3cur3t0k3n=="
   endpoint    = "https://api.example.org/v1"
   timeout     = 60
